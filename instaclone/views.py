f2e842ae2a393d05ba324dab7e543b690eecb6a1
jerumanu <emmanueljeru200@email.com>
2021-01-16 11:47:26 +0300

    add forms

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import NewsLetterForm
+#......
+def news_today(request):
+#........
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
