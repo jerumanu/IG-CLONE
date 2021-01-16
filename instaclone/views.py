62c32184a30fddba74c33de6c598d4d80e0528eb
jerumanu <emmanueljeru200@email.com>
2021-01-16 11:55:12 +0300

    edit views

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 
 from .forms import NewsLetterForm
 #......
 def news_today(request):
-#........
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
-    return render(request, 'index.html', {"date": date,"news":news,"letterForm":form})
+    return render(request, 'index-news.html', {"date": date,"news":news,"letterForm":form})
