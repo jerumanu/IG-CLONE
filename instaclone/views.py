f1ed686fed58f86bdbc12bdc41afed328ca5395b
jerumanu <emmanueljeru200@email.com>
2021-01-16 16:06:18 +0300

    add logout fuction

--- instaclone/views.py
+++ instaclone/views.py
@@ -4,18 +4,3 @@ from django.contrib.auth.decorators import login_required
 # from .forms import NewsLetterForm
 # #......
 @login_required(login_url='/accounts/login/')
-
-# def news_today(request):
-#     if request.method == 'POST':
-#         form = NewsLetterForm(request.POST)
-#         if form.is_valid():
-#             name = form.cleaned_data['your_name']
-#             email = form.cleaned_data['email']
-#             recipient = NewsLetterRecipients(name = name,email =email)
-#             recipient.save()
-#             HttpResponseRedirect('news_today')
-#     else:
-#         form = NewsLetterForm()
-#     return render(request, 'index-news.html', {"date": date,"news":news,"letterForm":form})
-
-
--- instagram/urls.py
+++ instagram/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 # from django.urls.conf import include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include ( include('registration.backends.simple.urls')))
+    path('accounts/',include ('registration.backends.simple.urls')),
+    path ('logout/$', views.logout, {"next_page": '/'}),
 ]
