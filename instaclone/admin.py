28d420224fcada0bea5c0ba9b4729ba054319826
jerumanu <emmanueljeru200@email.com>
2021-01-18 11:03:35 +0300

    added admin

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# from __future__ import unicode_literals
+from .models import Image,Profile,Comment
+
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
