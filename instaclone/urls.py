8169a0808cd4e38f12eed78680d3aef756481f3c
jerumanu <emmanueljeru200@email.com>
2021-01-18 11:04:52 +0300

    add path

---
+++ instaclone/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path,re_path
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns =[
+    path('', views.index, name = 'newsToday'),
+    path('search/', views.search_results, name='search_results'),
+    path('new/comment/', views.new_comment, name='new_comment'),
+    path('new/post/', views.new_post, name='new_post'),
+    path('profile/', views.new_profile, name='new_profile'),
+    path('image/(\d+)', views.get_image, name='image_results'),
+    path('like/<int:pk>/', views.like_image, name='like_post'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
